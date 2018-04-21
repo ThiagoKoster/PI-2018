49d60151f705609650531f1d17d7930dff2dfab9
Thiago Koster Lago <thiagokoster@gmail.com>
2018-04-21 19:59:32 -0300

    menu.py criado, remoção de funções referentes ao rtc

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO # Raspberry GPIO module
 import time
 from datetime import datetime
-import SDL_DS3231 #RTC module
 import GPIO_CONTROL #Our module to control peripherals connected to the gpios (buttons, lcd, motor, buzzer) 
 ################################# Function Definitions ###########################
 
@@ -10,12 +9,9 @@ import GPIO_CONTROL #Our module to control peripherals connected to the gpios (b
 #Main program
 try:
     motor = GPIO_CONTROL.Motor() # start motor object with default values ( GPIO = 21 , BCM_MODE)
-    ds3231 = SDL_DS3231.SDL_DS3231() # start rtc object with default values
-    ds3231.write_now()
     
     #Main loop
     while True:
-        print ( ("DS3231=\t\t%s") % ds3231.read_datetime())
         motor.turnOn()
         print( str(datetime.now()) + " | Motor On" )  # DEBUG
         time.sleep(2)
@@ -24,7 +20,8 @@ try:
         time.sleep(2)
 
 except KeyboardInterrupt:
-    print("Program Stopped")
+    print("\nProgram Stopped")
+    import menu 
     GPIO.cleanup()
 
 except SystemExit:
