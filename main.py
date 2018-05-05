acfc54290cedeb6498bcb028f4e22663250a05d8
Anor Neto <anornetoo@gmail.com>
2018-05-05 11:38:36 -0300

    bug fixes on main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from datetime import datetime,time
 from time import sleep
 import GPIO_CONTROL #Our module to control peripherals connected to the gpios (buttons, lcd, motor, buzzer) 
 ################################# Function Definitions ###########################
+
 #void -> int , array of datetime.time objects or a single datetime.time object 
 def getSelectedTimes():
 
     fileLines = open('horarios.txt', 'r').read().splitlines() # put each line of the file in an list of strings
     selectedTimes = [] #list of datetime objects
-    timesString = fileLines[1].split(" ")    
+    timesString = fileLines[1].split(" ") 
     print(timesString)
     for i in range(len(timesString)):
-        selectedTimes.append(datetime.strptime(timesString[i],"%H:%M"))
+        selectedTimes.append(datetime.strptime(timesString[i],"%H:%M").time())
     print(selectedTimes)
-    return int(fileLines[0]), selectedTimes
+    return int(fileLines[0]), selectedTimes # option, selectedTImes
     #return int(fileLines[0]), selectedTimes # selectedTimes = array of datetime.time OR a single datetime.time object 
    
 
@@ -21,22 +22,22 @@ def getSelectedTimes():
 
 #Main program
 try:
-    motor = GPIO_CONTROL.Motor() # start motor object with default values ( GPIO = 21 , BCM_MODE)
+    #motor = GPIO_CONTROL.Motor() # start motor object with default values ( GPIO = 21 , BCM_MODE)
     
     
     option,selectedTimes = getSelectedTimes()
 
     #Main loop for option 1
     if option == 1:
+        lastActiveTime = 0 # initialize lastActiveTime
         while True:
             timeNow = datetime.now().time()
             timeNow = timeNow.replace(second=0 , microsecond=0) # cut out seconds and microseconds
-            lastActive = time(0,0)
             for i in selectedTimes:
-                if lastActive == timeNow: # this should prevent from activating more than once
-                        break             # on the same minutes but it does not, why???
-                elif timeNow == i :     #maybe put line 39 here with an AND?
-                    lastActive = timeNow
+                if lastActiveTime == timeNow: #prevent from activating more than once on the same minute
+                        break             
+                elif timeNow == i : 
+                    lastActiveTime = timeNow
                     motor.turnOn()
                     print( str(datetime.now()) + " | Motor On" )  # DEBUG
                     sleep(2)
