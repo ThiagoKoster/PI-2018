7626e943d1e5d7630dfb6fbc721db817f67d3d0c
Anor Neto <anornetoo@gmail.com>
2018-05-05 16:21:54 -0300

    revamp of main.py , everything working

--- main.py
+++ main.py
@@ -1,63 +1,81 @@
 import RPi.GPIO as GPIO # Raspberry GPIO module #DEBUG
-from datetime import datetime,time
+from datetime import datetime,time,timedelta
 from time import sleep
+import sys
 import GPIO_CONTROL #Our module to control peripherals connected to the gpios (buttons, lcd, motor, buzzer) #DEBUG
 ################################# Function Definitions ###########################
 
-#void -> int , array of datetime.time objects or a single datetime.time object 
+#void -> list of datetime.time objects
 def getSelectedTimes():
 
     fileLines = open('horarios.txt', 'r').read().splitlines() # put each line of the file in an list of strings
     selectedTimes = [] #list of datetime objects
+    option = fileLines[0] # option is on the first line
     timesString = fileLines[1].split(" ") 
-    print(timesString)
-    for i in range(len(timesString)):
-        selectedTimes.append(datetime.strptime(timesString[i],"%H:%M").time())
-    print(selectedTimes)
-    return int(fileLines[0]), selectedTimes # option, selectedTImes
-    #return int(fileLines[0]), selectedTimes # selectedTimes = array of datetime.time OR a single datetime.time object 
-   
 
+    for i in range(len(timesString)): #convert timeString on a list of datetime.datetime objects
+        selectedTimes.append(datetime.strptime(timesString[i],"%H:%M")) 
+
+    if option == '1': # option 1 - user gives the schedule
+        selectedTimes = toTimeObject(selectedTimes)
+    elif option == '2':# option 2 - user gives a start time and an interval and we generate a schedule based on that
+        selectedTimes = generateSchedule(selectedTimes[0],selectedTimes[1]) # ( startTime , interval )
+        selectedTimes = toTimeObject(selectedTimes) #we can only use timedelta arithmetic on datetime.datetime objects
+        #thats why we convert it later to datetime.time
+    else:
+        print('ERROR : Arquivo horarios.txt está formatado incorretamente')
+        sys.exit()
+    return option,selectedTimes # option, selectedTImes
+
+#converts a list of datetime.time to datetime.time objects
+# list datetime.datetime objects -> list datetime.time objects
+def toTimeObject(selectedTimes):
+    for i in range(len(selectedTimes)):
+        selectedTimes[i] = selectedTimes[i].time().replace(second=0,microsecond=0) #convert datetime to time cutting out seconds and microseconds
+    return selectedTimes
+
+#generates a schedule based on a start time and an interval
+# datetime.datetime object , datetime.datetime object -> list datetime.datetime objects
+def generateSchedule(startTime, interval):
+    schedule = []
+    endTime = startTime + timedelta(days=1)
+    delta = timedelta(hours = interval.hour , minutes = interval.minute)
+    while startTime < endTime:
+        schedule.append(startTime)
+        startTime += delta
+    return schedule
+    
 ####################################################
 
 #Main program
 try:
     motor = GPIO_CONTROL.Motor() # start motor object with default values ( GPIO = 21 , BCM_MODE) #DEBUG
     
-    
     option,selectedTimes = getSelectedTimes()
 
-    #Main loop for option 1
-    if option == 1:
-        lastActiveTime = 0 # initialize lastActiveTime
-        while True:
-            timeNow = datetime.now().time()
-            timeNow = timeNow.replace(second=0 , microsecond=0) # cut out seconds and microseconds
-            for i in selectedTimes:
-                if lastActiveTime == timeNow: #prevent from activating more than once on the same minute
-                        break             
-                elif timeNow == i : 
-                    lastActiveTime = timeNow
-                    motor.turnOn() #DEBUG
-                    print( str(datetime.now()) + " | Motor On" )  
-                    sleep(2)
-                    motor.turnOff() #DEBUG
-                    print( str(datetime.now()) + " | Motor Off" ) 
-                    sleep(2)
-            sleep(20) #lower cpu ultilization inside the while loop <- improve this
-
-    #Main loop for option 2
-    elif option == 2: # another loop to treat option 2 ( dont know how yet) or generate the times that we want to activate based on
-        while True:     # the interval given
-            print('Under Construction')
-            sleep(30)
-
-
-
+    lastActiveTime = 0 # initialize lastActiveTime
+    while True:
+        ###### TODO  ######
+        # Print Option Selected and Schedule? dont know if its really necessary
+        timeNow = datetime.now().time().replace(second=0 , microsecond=0) # cut out seconds and microseconds
+        for i in selectedTimes:
+            if lastActiveTime == timeNow: #prevent from activating more than once on the same minute
+                break             
+            elif timeNow == i : 
+                lastActiveTime = timeNow
+                motor.turnOn() #DEBUG
+                print( str(datetime.now()) + " | Motor On" )  
+                sleep(2)
+                motor.turnOff() #DEBUG
+                print( str(datetime.now()) + " | Motor Off" ) 
+                sleep(2)
+        sleep(20) #lower cpu ultilization inside the while loop <- improve this
+  
+            
 except KeyboardInterrupt:
     print("\nProgram Stopped")
     GPIO.cleanup() #DEBUG
-    import menu
+    import menu_debug
 
 except SystemExit:
     GPIO.cleanup() #DEBUG
