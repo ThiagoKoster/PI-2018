8b77ed94a49a2903d475542f5ed3b7cce65a244a
Thiago Koster Lago <thiagokoster@gmail.com>
2018-05-09 20:35:37 -0300

    Criacao função feedPet

--- GPIO_CONTROL.py
+++ GPIO_CONTROL.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
-
+import time
 class Motor():
-
+    motorRPM = 3
     #Initializations
     def __init__(self, pinNumber = 21, pinMode = GPIO.BCM):
         self.pinNumber = pinNumber
@@ -17,6 +17,18 @@ class Motor():
     #Function to turn the motor off
     def turnOff(self):
         GPIO.output(self.pinNumber, GPIO.LOW)
+
+
+
+    def feedPet(rotations):
+        #time of motor operation
+        time = rotations/motorRPM
+        turnOn()
+        print( str(datetime.now()) + " | Motor On" )  
+        sleep(time)
+        turnOff()
+        print( str(datetime.now()) + " | Motor Off" ) 
+
 
     #maybe a function to control speed  if needed?
 
